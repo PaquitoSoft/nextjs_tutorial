880ca8756df5411230c1d2b9ca8f53fd85d42ce4
paquitosoft <paquitosoftware@gmail.com>
2020-10-17 19:50:15 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+// This is to remove unused CSS
+// https://tailwindcss.com/docs/controlling-file-size#removing-unused-css
+module.exports = {
+	purge: [
+		'./pages/**/.js',
+		'./components/**/*.js'
+	]
+}
